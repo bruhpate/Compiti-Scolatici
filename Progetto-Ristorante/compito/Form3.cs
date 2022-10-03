4daff57608ef468189d01c773d5ac8a902406e3a
bruhpate <pietropatelli05@gmail.com>
2022-10-03 08:29:07 +0200

    Aggiunta compito ristorante

---
+++ Progetto-Ristorante/compito/Form3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace compito
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void Scrivi(string x)
+        {
+            var oStream = new FileStream("./menu.txt", FileMode.Append, FileAccess.Write, FileShare.Read);
+            StreamWriter sw = new StreamWriter(oStream);
+            sw.WriteLine(x);
+            sw.Close();
+            oStream.Close();
+        }
+
+        public void componi()
+        {
+            string x;
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                try
+                {
+                    decimal y = Convert.ToDecimal(textBox2.Text);
+                    string sep = ";";
+                    x = textBox1.Text + sep + textBox2.Text + sep + textBox3.Text + sep + textBox4.Text + sep + "True";
+                    Scrivi(x);
+                    this.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Inserisci un costo valido");
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            componi();
+        }
+    }
+}
